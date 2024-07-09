d1d222ff78071dd0a12af68309b20bb7569407ec
jHoon0223 <aleejaehun@naver.com>
2024-07-09 13:37:02 +0900

    240709 : first commit

---
+++ React/Node.js/hello.js
@@ -0,0 +1,14 @@
+// function hello(name) {
+//     console.log(name + " 님, 안녕하세요?");
+// }
+ 
+// hello("홍길동");
+
+// let abc;
+// abc = 1;
+// console.log(abc);
+
+let num = "10a" + 54;
+const str = "asdasd22";
+//str = "23";
+console.log({num, str});
